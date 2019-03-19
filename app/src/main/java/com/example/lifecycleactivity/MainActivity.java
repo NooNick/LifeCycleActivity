c8c686568118df2961e15c67d3700386bf7cb55c
Чуканов Юрий <noonick@mail.ru>
2019-03-19 14:51:39 +0400

    ДЗ по 2 уроку. тоже с опозданием.

--- app/src/main/java/com/example/lifecycleactivity/MainActivity.java
+++ app/src/main/java/com/example/lifecycleactivity/MainActivity.java
@@ -3,12 +3,71 @@ package com.example.lifecycleactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String instanceState;
+        if(savedInstanceState == null) {
+            instanceState = "Первый Запуск!";
+        }else {
+            instanceState = "Повторный запуск!";
+        }
+        Log.i(TAG,instanceState + " - onCreate() ");
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart()");
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        Log.i(TAG,"Повторный запуск!! - onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        Log.i(TAG,"onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy()");
+    }
+
 }
